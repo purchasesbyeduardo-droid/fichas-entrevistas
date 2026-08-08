11b23fbc7f7b85921217ca0902748060dcb96fe3
purchasesbyeduardo-droid <purchasesbyeduardo@gmail.com>
2026-08-08 15:36:20 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'entrevistas-v1';
+var CACHE = 'entrevistas-v3';
 var ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e){
