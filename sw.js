ba7837c7e49f7fbb90a5e96b77fde607b11eb512
purchasesbyeduardo-droid <purchasesbyeduardo@gmail.com>
2026-08-11 16:02:16 -0300

    Update cache version from v4 to v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'entrevistas-v4';
+var CACHE = 'entrevistas-v5';
 var ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e){
